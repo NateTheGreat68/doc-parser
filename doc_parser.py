1f77ce504c0198519528c4ef2ae46e0c1d389a8b
Nathan Freeman <natethegreat68.dev@gmail.com>
2022-11-13 22:26:03 -0500

    Refactored to create OutputData class.
    Nesting OutputData within ParserCollection makes a much cleaner
    way to get both data field names ("props") and the records.
    Before, getting the data field names was going to be clunky and
    repetitive.

--- doc_parser.py
+++ doc_parser.py
@@ -127,15 +127,32 @@ class ParserCollection:
         self.docs[self._next_key] = self.parser(filepath)
         self._next_key += 1
 
-    def output_dicts(
+    def output_data(
             self,
-            props: List[str] = None,
-            extended_attr: str = None,
-            ) -> Generator[Dict, None, None]:
+            **kwargs,
+            ) -> 'ParserCollection.OutputData':
         """
-        Creates a dict for each doc in the collection with the selected properties.
+        A factory for creating ParserCollection.OutputData class.
+        See that class for additional information on usage.
 
         Parameters:
+          - **kwargs: See the init for ParseCollection.OutputData
+            for information on parameters.
+        Return: A built ParserCollection.OutputData class.
+        """
+        return ParserCollection.OutputData(
+                self,
+                **kwargs,
+                )
+
+
+    class OutputData:
+        """
+        Used to create output data which can be consumed by other methods to
+        output csv files, SQL tables, etc.
+
+        Init Parameters:
+          - parser_collection: The instance of the outer ParserCollection class.
           - props: Optional. A list of the parser attrs to be output in each dict.
             If not provided, the output_props specified at class initialization is
             used.
@@ -143,33 +160,64 @@ class ParserCollection:
             fetching "extended" data (data for which there are 0 or more records
             per document, rather than exactly one record per document). When None
             (default), doesn't act as extended data.
-        Return: Generator, each iteration yielding a dict with props as keys and
-        the parser's related attrs as values. Each dict includes a "key" value
-        (when not in extended mode) or a "doc_key" value (when in extended mode).
         """
-        if not props:
-            if not extended_attr:
-                props = self.output_props
-            else:
-                props = self.output_props_extended[extended_attr]
-        for key, doc in self.docs.items():
-            key = getattr(doc, 'key', key) #Use the parser's key attr if it has it
-            if not extended_attr: #Fetch doc attrs (non-extended mode)
-                try:
-                    output = {}
-                    output['key'] = key
-                    for prop in [p for p in props if p != 'key']:
-                        output[prop] = getattr(doc, prop, None)
-                    yield output
-                except:
-                    continue
-            else: #Fetch 0 or more records from a single doc attr (extended mode)
-                try:
-                    for record in getattr(doc, extended_attr, []): #Empty list default to prevent error
+
+        def __init__(
+                self,
+                parser_collection: 'ParserCollection',
+                props: List[str] = None,
+                extended_attr: str = None,
+                ):
+            self.parser_collection = parser_collection
+            if not props:
+                if not extended_attr:
+                    self.props = parser_collection.output_props
+                else:
+                    self.props = parser_collection.output_props_extended[extended_attr]
+            self.extended_attr = extended_attr
+
+        @property
+        def field_list(
+                self,
+                ) -> List[str]:
+            """
+            Provides a field list for the compiled output data.
+
+            Return: A list of the fields (props) provided by this OutputData.
+            """
+            return self.props
+
+        @property
+        def records(
+                self,
+                ) -> Generator[Dict, None, None]:
+            """
+            Creates a dict for each doc in the collection with the selected properties.
+
+            Return: Generator, each iteration yielding a dict with props as keys and
+            the parser's related attrs as values. Each dict includes a "key" value
+            (when not in extended mode) or a "doc_key" value (when in extended mode).
+            """
+            props = self.props
+            extended_attr = self.extended_attr
+            for key, doc in self.parser_collection.docs.items():
+                key = getattr(doc, 'key', key) #Use the parser's key attr if it has it
+                if not extended_attr: #Fetch doc attrs (non-extended mode)
+                    try:
                         output = {}
-                        output['doc_key'] = key
-                        for prop in [p for p in props if p != 'doc_key']:
-                            output[prop] = record[prop] if prop in record else None
+                        output['key'] = key
+                        for prop in [p for p in props if p != 'key']:
+                            output[prop] = getattr(doc, prop, None)
                         yield output
-                except:
-                    continue
+                    except:
+                        continue
+                else: #Fetch 0 or more records from a single doc attr (extended mode)
+                    try:
+                        for record in getattr(doc, extended_attr, []): #Empty list default to prevent error
+                            output = {}
+                            output['doc_key'] = key
+                            for prop in [p for p in props if p != 'doc_key']:
+                                output[prop] = record[prop] if prop in record else None
+                            yield output
+                    except:
+                        continue
